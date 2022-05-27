adfa29acd3526b4f67b34ec825108f68a9ac21db
ashidiqidimas <prabowo6@gmail.com>
2022-05-27 19:49:27 +0700

    Finished navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,47 @@
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: ["./*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./*.{html,js}"],
+    theme: {
+        container: {
+            center: true,
+        },
+        fontSize: {
+          'base': ['1rem', '1rem'],
+        },
+        screens: {
+            'tablet': '768px',
+            'laptop': '1366px',
+        },
+        colors: {
+            gray: colors.slate,
+            white: colors.white,
+        },
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primaryColor: {
+                    50: '#E2F1FF',
+                    100: '#B9DCFF',
+                    200: '#8AC6FF',
+                    300: '#53B0FF',
+                    400: '#229DFF',
+                    500: '#008CFF',
+                    600: '#117DFF',
+                    700: '#2369F6',
+                    800: '#2A56E3',
+                    900: '#3231C3',
+                },
+                textColor: {
+                    'primary': '#0A1841',
+                    'secondary': '#284575',
+                    'tertiary': '#5E789C',
+                }
+            },
+        },
+    },
+    plugins: [],
 }
